4971fbe99cbcddc51f8dd127f47ca1bac6f4708b
nphuyy1901 <66677307+nphuyy1901@users.noreply.github.com>
2021-12-31 20:01:53 +0700

    Form Quan Tri

--- Code/CSDLNC_CosplayBanHoa/QuanTri/LichSuNhap_QT.Designer.cs
+++ Code/CSDLNC_CosplayBanHoa/QuanTri/LichSuNhap_QT.Designer.cs
@@ -1,4 +1,5 @@
 ﻿
+﻿
 namespace CSDLNC_CosplayBanHoa
 {
     partial class LichSuNhap_QT
@@ -30,35 +31,37 @@ namespace CSDLNC_CosplayBanHoa
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dGV_DoiTac_SP = new System.Windows.Forms.DataGridView();
+            this.dGV_QT_LSN = new System.Windows.Forms.DataGridView();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker_QT_NGAYNHAP = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_QT_SOLUONG = new System.Windows.Forms.TextBox();
+            this.textBox_QT_NGUOINHAP = new System.Windows.Forms.TextBox();
+            this.textBox_QT_MASP = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dGV_DoiTac_SP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_QT_LSN)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dGV_DoiTac_SP
+            // dGV_QT_LSN
             // 
-            this.dGV_DoiTac_SP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGV_DoiTac_SP.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dGV_DoiTac_SP.Location = new System.Drawing.Point(0, 393);
-            this.dGV_DoiTac_SP.Name = "dGV_DoiTac_SP";
-            this.dGV_DoiTac_SP.RowHeadersWidth = 51;
-            this.dGV_DoiTac_SP.RowTemplate.Height = 24;
-            this.dGV_DoiTac_SP.Size = new System.Drawing.Size(1136, 282);
-            this.dGV_DoiTac_SP.TabIndex = 13;
+            this.dGV_QT_LSN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV_QT_LSN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dGV_QT_LSN.Location = new System.Drawing.Point(0, 491);
+            this.dGV_QT_LSN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dGV_QT_LSN.Name = "dGV_QT_LSN";
+            this.dGV_QT_LSN.RowHeadersWidth = 51;
+            this.dGV_QT_LSN.RowTemplate.Height = 24;
+            this.dGV_QT_LSN.Size = new System.Drawing.Size(1278, 353);
+            this.dGV_QT_LSN.TabIndex = 13;
+            this.dGV_QT_LSN.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGV_QT_LSN_CellClick);
             // 
             // imageList1
             // 
@@ -71,9 +74,9 @@ namespace CSDLNC_CosplayBanHoa
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label6.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label6.Location = new System.Drawing.Point(355, 124);
+            this.label6.Location = new System.Drawing.Point(399, 155);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(131, 25);
+            this.label6.Size = new System.Drawing.Size(156, 29);
             this.label6.TabIndex = 15;
             this.label6.Text = "Mã sản phẩm";
             // 
@@ -82,9 +85,9 @@ namespace CSDLNC_CosplayBanHoa
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(529, 17);
+            this.label1.Location = new System.Drawing.Point(595, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 32);
+            this.label1.Size = new System.Drawing.Size(247, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "LỊCH SỬ NHẬP";
             // 
@@ -92,11 +95,11 @@ namespace CSDLNC_CosplayBanHoa
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dateTimePicker_QT_NGAYNHAP);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.textBox4);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox_QT_SOLUONG);
+            this.panel1.Controls.Add(this.textBox_QT_NGUOINHAP);
+            this.panel1.Controls.Add(this.textBox_QT_MASP);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
@@ -105,18 +108,83 @@ namespace CSDLNC_CosplayBanHoa
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1136, 393);
+            this.panel1.Size = new System.Drawing.Size(1278, 491);
             this.panel1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Location = new System.Drawing.Point(864, 220);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 44);
+            this.button2.TabIndex = 99;
+            this.button2.Text = "Tìm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker_QT_NGAYNHAP
+            // 
+            this.dateTimePicker_QT_NGAYNHAP.CustomFormat = "";
+            this.dateTimePicker_QT_NGAYNHAP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.dateTimePicker_QT_NGAYNHAP.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_QT_NGAYNHAP.Location = new System.Drawing.Point(569, 229);
+            this.dateTimePicker_QT_NGAYNHAP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker_QT_NGAYNHAP.MaximumSize = new System.Drawing.Size(276, 35);
+            this.dateTimePicker_QT_NGAYNHAP.MinimumSize = new System.Drawing.Size(4, 33);
+            this.dateTimePicker_QT_NGAYNHAP.Name = "dateTimePicker_QT_NGAYNHAP";
+            this.dateTimePicker_QT_NGAYNHAP.Size = new System.Drawing.Size(276, 35);
+            this.dateTimePicker_QT_NGAYNHAP.TabIndex = 98;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(864, 142);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 44);
+            this.button1.TabIndex = 95;
+            this.button1.Text = "Tìm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_QT_SOLUONG
+            // 
+            this.textBox_QT_SOLUONG.Location = new System.Drawing.Point(569, 394);
+            this.textBox_QT_SOLUONG.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_QT_SOLUONG.Multiline = true;
+            this.textBox_QT_SOLUONG.Name = "textBox_QT_SOLUONG";
+            this.textBox_QT_SOLUONG.Size = new System.Drawing.Size(276, 43);
+            this.textBox_QT_SOLUONG.TabIndex = 90;
+            // 
+            // textBox_QT_NGUOINHAP
+            // 
+            this.textBox_QT_NGUOINHAP.Location = new System.Drawing.Point(569, 310);
+            this.textBox_QT_NGUOINHAP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_QT_NGUOINHAP.Multiline = true;
+            this.textBox_QT_NGUOINHAP.Name = "textBox_QT_NGUOINHAP";
+            this.textBox_QT_NGUOINHAP.Size = new System.Drawing.Size(276, 43);
+            this.textBox_QT_NGUOINHAP.TabIndex = 89;
+            // 
+            // textBox_QT_MASP
+            // 
+            this.textBox_QT_MASP.Location = new System.Drawing.Point(569, 142);
+            this.textBox_QT_MASP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox_QT_MASP.Multiline = true;
+            this.textBox_QT_MASP.Name = "textBox_QT_MASP";
+            this.textBox_QT_MASP.Size = new System.Drawing.Size(276, 43);
+            this.textBox_QT_MASP.TabIndex = 87;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label5.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label5.Location = new System.Drawing.Point(396, 325);
+            this.label5.Location = new System.Drawing.Point(446, 406);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.Size = new System.Drawing.Size(109, 29);
             this.label5.TabIndex = 24;
             this.label5.Text = "Số lượng";
             // 
@@ -125,9 +193,9 @@ namespace CSDLNC_CosplayBanHoa
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label4.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label4.Location = new System.Drawing.Point(374, 258);
+            this.label4.Location = new System.Drawing.Point(421, 322);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.Size = new System.Drawing.Size(137, 29);
             this.label4.TabIndex = 22;
             this.label4.Text = "Người nhập";
             // 
@@ -136,9 +204,9 @@ namespace CSDLNC_CosplayBanHoa
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label3.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label3.Location = new System.Drawing.Point(379, 191);
+            this.label3.Location = new System.Drawing.Point(426, 239);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 25);
+            this.label3.Size = new System.Drawing.Size(128, 29);
             this.label3.TabIndex = 20;
             this.label3.Text = "Ngày nhập";
             // 
@@ -147,78 +215,23 @@ namespace CSDLNC_CosplayBanHoa
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label2.Location = new System.Drawing.Point(388, 226);
+            this.label2.Location = new System.Drawing.Point(436, 282);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.Size = new System.Drawing.Size(0, 29);
             this.label2.TabIndex = 17;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(506, 114);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 35);
-            this.textBox1.TabIndex = 87;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(506, 248);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(246, 35);
-            this.textBox3.TabIndex = 89;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(506, 315);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(246, 35);
-            this.textBox4.TabIndex = 90;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button1.Location = new System.Drawing.Point(768, 114);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 35);
-            this.button1.TabIndex = 95;
-            this.button1.Text = "Tìm";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CustomFormat = "";
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(506, 183);
-            this.dateTimePicker1.MaximumSize = new System.Drawing.Size(246, 35);
-            this.dateTimePicker1.MinimumSize = new System.Drawing.Size(4, 33);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(246, 33);
-            this.dateTimePicker1.TabIndex = 98;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.button2.Location = new System.Drawing.Point(768, 186);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(80, 35);
-            this.button2.TabIndex = 99;
-            this.button2.Text = "Tìm";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // LichSuNhap_QT
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1136, 675);
-            this.Controls.Add(this.dGV_DoiTac_SP);
+            this.ClientSize = new System.Drawing.Size(1278, 844);
+            this.Controls.Add(this.dGV_QT_LSN);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "LichSuNhap_QT";
             this.Text = "LichSuNhap_QT";
-            ((System.ComponentModel.ISupportInitialize)(this.dGV_DoiTac_SP)).EndInit();
+            this.Load += new System.EventHandler(this.LichSuNhap_QT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGV_QT_LSN)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -227,7 +240,7 @@ namespace CSDLNC_CosplayBanHoa
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dGV_DoiTac_SP;
+        private System.Windows.Forms.DataGridView dGV_QT_LSN;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label1;
@@ -236,11 +249,11 @@ namespace CSDLNC_CosplayBanHoa
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_QT_SOLUONG;
+        private System.Windows.Forms.TextBox textBox_QT_NGUOINHAP;
+        private System.Windows.Forms.TextBox textBox_QT_MASP;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_QT_NGAYNHAP;
     }
 }
