5931b23a0a8a34213884db9aca335a1f6a668231
himneh <84070553+himneh@users.noreply.github.com>
2022-01-01 20:33:36 +0700

    Update Trigger.sql

--- Script/Trigger.sql
+++ Script/Trigger.sql
@@ -24,10 +24,9 @@ GO
        -- update tong tien 
        begin
        update DONHANG
-        set TONGTIEN = TONGTIEN + I.THANHTIEN
-		--+ (select CTHD.THANHTIEN  
-  --                                from inserted I,CT_DONHANG CTHD
-  --                                where DH.MADH=CTHD.MADH and CTHD.MADH=I.MADH and I.MASP=CTHD.MASP)
+        set TONGTIEN = (SELECT SUM(CT.THANHTIEN)
+						FROM CT_DONHANG CT JOIN DONHANG DH ON DH.MADH = CT.MADH
+						WHERE DH.MADH = I.MADH)
        from DONHANG DH, inserted I
        where DH.MADH = I.MADH
        end 
@@ -81,6 +80,65 @@ Create trigger delete_CT_DONHANG_TongTien
 						FROM CT_DONHANG CT JOIN DONHANG DH ON DH.MADH = CT.MADH
 						WHERE DH.MADH = DH1.MADH)
        from DONHANG DH1 join deleted D on DH1.MADH = D.MADH
+    end 
+  
+GO
+-------------------------------------------------
+
+--Tạo trigger để tự cập nhật số lượng tồn của sản phẩm khi thêm chi tiết hoá đơn
+-- drop trigger Insert_SANPHAM_SOLUONGTON
+ Create trigger Insert_SANPHAM_SOLUONGTON
+    on CT_DONHANG
+    for insert
+    as
+    begin
+       -- update SO LUONG TON
+    	update SANPHAM
+    	 set SOLUONGTON = SOLUONGTON - I.SOLUONG
+    	from inserted I, 
+    		 CT_DONHANG CTHD,
+    		 SANPHAM SP
+
+        --Chi tiết hóa đơn mới thêm vào phải tồn tại sản phẩm, và mã hóa đơn
+    	where SP.MASP=I.MASP and SP.MASP=CTHD.MASP 
+    end 
+  
+GO
+
+--Tạo trigger để tự cập nhật SỐ LƯỢNG TỒN Ở SẢN PHẨM KHI THAY chi tiết hóa đơn 
+
+  --drop trigger update_CT_DONHANG_ThanhTien_TongTien
+Create trigger update_SOLUONGTON_SANPHAM
+    on CT_DONHANG
+    for update
+    as
+    begin
+       --update số lượng ở chi tiết hóa đơn
+    if UPDATE(SOLUONG)
+       begin
+       update SANPHAM
+		set SOLUONGTON = SOLUONGTON - I.SOLUONG + d.SOLUONG
+    	from inserted I, 
+			deleted d,
+    		 CT_DONHANG CTHD,
+    		 SANPHAM SP
+
+    	where SP.MASP = I.MASP and SP.MASP = CTHD.MASP and d.masp = sp.masp
+		end
+	end
+GO
+
+-- TẠO TRIGGER cập nhật số lượng tồn khi xóa CT hóa đơn
+  --drop trigger delete_CT_DONHANG_TongTien
+Create trigger delete_SANPHAM_SOLUONGTON
+    on CT_DONHANG
+    for delete
+    as
+    begin
+       -- cập nhật lại tổng tiền khi xóa CT hóa đơn
+       update SANPHAM
+       set SOLUONGTON = SOLUONGTON + D.SOLUONG
+       from SANPHAM SP join deleted D on SP.MASP = D.MASP
     end 
   
 GO
